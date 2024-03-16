edc73b4b6d16d2296e226a791b0199187c6f636f
Wensi (Vince) Ai <vinceai@outlook.com>
2024-03-15 19:19:28 -0700

    Update v0.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # read the contents of your README file
 from os import path
-
-from setuptools import setup
+from setuptools import find_packages, setup
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
@@ -11,9 +10,10 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 lines = [x for x in lines if ".png" not in x]
 long_description = "".join(lines)
 
+
 setup(
     name="telemoma",
-    version="0.0.1",
+    version="0.1.0",
     author="Shivin Dass",
     author_email='shivindass@gmail.com',
     description='A modular and versatile teleoperation system for mobile manipulation',
@@ -22,16 +22,15 @@ setup(
     url="https://github.com/UT-Austin-RobIn/telemoma",
     install_requires=[
         "numpy~=1.23.5",
-        "opencv-python~=4.8.1.78",
+        "opencv-python~=4.8.1",
         "mediapipe~=0.10.9",
         "scipy~=1.10.1",
-        "pyrealsense2",
-        "pyspacemouse",
+        "pyrealsense2~=2.54.2.5684",
+        "pyspacemouse~=1.1.1",
         "pynput~=1.7.6",
-        "matplotlib~=3.7.1",
+        "matplotlib~=3.8.2",
         "gym~=0.26.2",
     ],
-    tests_require=[],
-    packages=['telemoma'],
+    packages=find_packages(),
     python_requires=">=3",
 ) 
